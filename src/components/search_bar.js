8a0ebdc51b147278fd8977eb901a6af017f69d8b
Gota <30895814+gotaSlick@users.noreply.github.com>
2018-01-18 12:22:30 +0100

    Update search_bar.js

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,6 @@
-// the input at the top of the page to look for videos
-// when the user types input, we need to make the api request to the youtube api.
-// {Component} here is exactly like: const Component = React.Component; Just making the code
-// cleaner, so that I dont have to write React.Component later, but just Component
+
 import React, { Component } from 'react';
 
-// all the class components must have their render method that returns some JSX
 class SearchBar extends Component {
     constructor(props) {
         super(props);
@@ -14,9 +10,6 @@ class SearchBar extends Component {
 
     render() {
         //whenever the input changes, run the code inside of the curly brackets
-        // we update the state with this.setState and pass it the object with it's property 
-        // and set the value that we want it to have now: event.target.value.
-        // whenever the input changes, it causes the state to be updated
         return (
             <div className="search-bar">
                 <input 
