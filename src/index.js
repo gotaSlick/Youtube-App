e7de7cc3c635bc72ac182f45059fc0c3c289d2e1
Gota <30895814+gotaSlick@users.noreply.github.com>
2018-01-18 12:16:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,33 +15,15 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-//-----------------EXLANATION------------------------------------------------ 
-// app boots up, videos is an empty array, selectedVideo is null.
-// we go into VideoDetail (in App render). While VideoDetail isn't provided
-// a video, it shows Loading... At the same time we kick off the API request
-// to fetch for some videos. When that request is completed, we'll pass the list of
-// videos onto this.state.videos and the first video of that list will be 
-// set on selectedVideo as [0].
-// Since we are setting state there, it causes our component to re-render, which means
-// VideoDetail will be rendered again with this.state.selectedVideo which is
-// now equal to the first video.
-// Then we have to implement our callback. The callback will be passed from App to
-// VideoList and then into VideoListItem
-//-------------------------------------------------------------------------- 
-            videos: [], // it'll be an array of videos
+
+            videos: [], // array of videos
             selectedVideo: null
         };
         this.videoSearch('surfing');
     }
-        //we call the package, its first argument is an object with 
-        //the API key and the search term, and the second argument is
-        //a callback function that gets called with some response data (here videos)
-        //'function (data) {}' same as arrow function '(data) => {}' 
+        
     videoSearch(term) {
     YTSearch({ key: API_KEY, term: term }, (videos) => {
-        //because data in this case is videos, it can be: ({ videos: videos }),
-        //and not ({ videos: data }) (videos is key and data is value)
-        //and this can be written more condensely: ({ videos }) in ES6
         this.setState({
             videos: videos,
             selectedVideo: videos[0]
@@ -49,7 +31,6 @@ class App extends Component {
     });
 }
 
-    // THE MAIN RENDER FUNCTION:
     render() {
         const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);//exactly like
         //google search works. The process is called: THROTTLING
@@ -58,8 +39,6 @@ class App extends Component {
             <SearchBar onSearchTermChange={videoSearch} />
             <VideoDetail video={this.state.selectedVideo} />
             <VideoList 
-        // onVideoSelect callback function that just updates the App's state,
-        // will be passed to video_list and then to video_list_item as a prop
                 onVideoSelect={selectedVideo => this.setState({selectedVideo})}
                 videos={this.state.videos} />
         </div>
@@ -67,10 +46,4 @@ class App extends Component {
     }
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
-// ReactDOM can take 2 arguments. 1st says: When you render this component (here App),
-// I want you to instert that into this element that already exist in my HTML document.
-// In my case here it's: <div class="container"></div> in index.html. It's basically
-// root node of my entire application, where I'll put all of my components.
-// This renders the App component into the div container. 
 ReactDOM.render(<App />, document.querySelector('.container'));
