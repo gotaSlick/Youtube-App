b0ad7d3fee9b42a4f294722e2c026e2cf373e873
Gota <30895814+gotaSlick@users.noreply.github.com>
2018-01-18 12:32:27 +0100

    Update video_list.js

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,14 +1,14 @@
-// this component will be a functional component, cause it doesn't have any need for state
+// functional component, as it doesn't have any need for state
 // it doesn't record any user interaction, or rerender itself in any way
 
 import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-    //for each element of videos we'll have a function that gets called 
-    //with a single video and will return a VideoListItem, and we'll pass it the
-    //video as a property named video. And we save a reference to this array that gets
-    //returned with const videoItems. We'll reference it as JSX in return {videoItems}
+    //for each element of videos there is a function that gets called 
+    //with a single video and will return a VideoListItem, it get passed the
+    //video as a property named video. A reference is saved (const videoItems) to this array that gets
+    //returned. In return {videoItems}
     const videoItems = props.videos.map((video) => {
         return (
         <VideoListItem 
